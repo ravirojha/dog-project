571a8ba8a1d1422aa77381b9ff5050b5131302ed
Ravi Ranjan Ojha <ravi.ranjan@bitcs.in>
2022-03-27 22:07:56 +0530

    Final commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { dogEntity } from './db/entities/dog.entity';
 import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [DogsModule],
+  imports: [DogsModule, TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'dogs.sqlite',
+    entities: [dogEntity],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { loadEnvironmentVariables } from './loader';
-import MainModule from './main.module';
+import { AppModule } from './app.module';
+
 
 async function bootstrap() {
-  await loadEnvironmentVariables();
-  console.log(process.env.TYPEORM_CONNECTION);
-  const app = await NestFactory.create(MainModule);
+
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
